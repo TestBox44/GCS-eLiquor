afde4657271fb6e5a20f8f5d2f6f510c02e7ac47
Gustavo1918 <gustavo.tuyo@unmsm.edu.pe>
2022-10-23 12:32:44 -0500

    He hecho un nuevo saludo

--- Desarrollo/SPVL/Fuentes/src/main/main.java
+++ Desarrollo/SPVL/Fuentes/src/main/main.java
@@ -14,5 +14,6 @@ public class main {
         System.out.println("Hola Sebastian");
         System.out.println("Hola Yosmar");
         System.out.println("Hola, soy Gustavo");
+        System.out.println("Prueba Gustavo");
     }
 }
