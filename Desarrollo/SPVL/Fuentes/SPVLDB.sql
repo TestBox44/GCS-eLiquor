dc340a74582c45707b5e3b873c473d2401c04b57
Sebastian Ortiz <iceglace12@hotmail.com>
2022-10-30 21:00:27 -0500

    Script de la base de datos

--- Desarrollo/SPVL/Fuentes/SPVLDB.sql
+++ Desarrollo/SPVL/Fuentes/SPVLDB.sql
@@ -1,7 +1,15 @@
 create database spvl;
 use spvl;
+desc usuarios;
 create table usuarios(
 	idUsuario int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    nombre varchar(45) NOT NULL
+    nombre varchar(45) NOT NULL,
+    PIN int NOT NULL,
+    gestionarVentas bool NOT NULL,
+    gestionarUsuarios bool NOT NULL,
+    gestionarProveedores bool NOT NULL,
+    gestionarClientes bool NOT NULL,
+    gestionarInventario bool NOT NULL,
+    generarReportes bool NOT NULL
 );
 select * from usuarios;
