8066e1e70b81d1027ee3bd02754817fed7dd97ce
Sebastian Ortiz <iceglace12@hotmail.com>
2022-11-04 07:44:52 -0500

    Módulos de Hito 1 Finalizados(Última modificación)

---
+++ Lineas Bases/SPVL/Linea Base 01/Fuentes/src/Presentacion/Interfaces/main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Presentacion.Interfaces;
+
+import Datos.DAO.UsuarioDAO;
+import Datos.Entidades.Usuario;
+import Presentacion.Interfaces.Login.LoginUsuarios;
+
+/**
+ *
+ * @author sortizu
+ */
+public class main {
+    public static void main(String[] args) {
+        FramePrincipal framePrincipal=new FramePrincipal();
+        framePrincipal.setVisible(true);
+    }
+    
+}
