f1d4f72d2d601c70adbf80b7f05abe803714007e
GerardoPazzz <gerardo.paz.an333@gmail.com>
2022-12-04 20:21:51 -0500

    Implementando Backend del Modulo Clientes

---
+++ Desarrollo/SPVL/Fuentes/src/Datos/DAO/ClienteDAO.java
@@ -0,0 +1,111 @@
+package Datos.DAO;
+
+import Datos.Entidades.Cliente;
+import java.util.List;
+import java.sql.*;
+import java.util.ArrayList;
+/**
+ *
+ * @author 
+ */
+public class ClienteDAO implements CRUD{
+    Conexion cn=new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    @Override
+    public int add(Object[] o) {
+        int r = 0;
+        int id=setLastId()+1;
+        String sql = 
+            "insert into cliente(nombre, correo, telefono, fechaRegistro,idCliente)values(?,?,?,?,?)";
+        try{
+            con = cn.Conectar();
+            ps = con.prepareStatement(sql);
+            ps.setObject(1, o[0]);
+            ps.setObject(2, o[1]);
+            ps.setObject(3, o[2]);
+            ps.setObject(4, o[3]);
+            ps.setObject(5, id);
+            r = ps.executeUpdate();
+        }catch(SQLException e){
+             System.out.println(e.toString());
+         }
+        return r;
+    }
+    
+
+    @Override
+    public List listar() {
+        List<Cliente> lista = new ArrayList<>();
+        String sql = "select * from cliente";
+        try{
+            con = cn.Conectar();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Cliente c = new Cliente();
+                c.setIdCliente(rs.getInt(1));
+                c.setNombre(rs.getString(2));
+                c.setTelefono(rs.getInt(3));
+                c.setCorreo(rs.getString(4));
+                c.setFechaRegistro(rs.getDate(5).toLocalDate());
+                lista.add(c);
+            }
+        }catch(SQLException e){
+             System.out.println(e.toString());
+         }
+        return lista;
+    }
+
+    @Override
+    public void eliminar(int id) {
+        String sql = "delete from cliente where idCliente=?";
+        try{
+           con = cn.Conectar();
+           ps = con.prepareStatement(sql);
+           ps.setInt(1,id);
+           ps.executeUpdate();
+       }catch(SQLException e){
+            System.out.println(e.toString());
+        }
+    }
+
+    @Override
+    public int actualizar(Object[] o) {
+        int r = 0;
+        String sql = "update cliente set nombre=?,correo=?,telefono=?,fechaRegistro=? where IdCliente=?";
+        try{
+           con = cn.Conectar();
+           ps = con.prepareStatement(sql);
+           ps.setObject(1, o[0]);
+           ps.setObject(2, o[1]);
+           ps.setObject(3, o[2]);
+           ps.setObject(4, o[3]);
+           ps.setObject(5, o[4]);
+           r = ps.executeUpdate();
+       }catch(SQLException e){
+            System.out.println(e.toString());
+        }
+       return r;
+    }
+    
+    public int setLastId(){
+        int id=1;
+       String sql = "SELECT MAX(idCliente) from cliente;";
+       try{
+           con = cn.Conectar();
+           ps = con.prepareStatement(sql);
+           rs = ps.executeQuery();
+           
+           rs.beforeFirst();
+           rs.next();
+           
+           id = rs.getInt(1);
+           
+       }catch(SQLException e){
+            System.out.println(e.toString());
+        }
+       return id;
+    } 
+}
